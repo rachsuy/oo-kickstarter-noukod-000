1501a277c102eb80951212d651399033a70f9653
Rachele Suy <rachelesuy@gmail.com>
2019-05-22 07:48:19 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,7 +8,7 @@ class Backer
   
   def back_project(project)
     @bracked_projects << project
-    project.add_bracker(self) unless project.brackers.include(self)
+    project.add_bracker(self) unless project.brackers.include?(self)
   end
   
 
