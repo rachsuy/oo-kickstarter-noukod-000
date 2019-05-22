1cc3a56b8ac12aeafc42f8f5a38c3554ed5e23dc
Rachele Suy <rachelesuy@gmail.com>
2019-05-22 07:52:58 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,13 @@
 class Backer 
-  attr_accessor :name, :bracked_projects
+  attr_accessor :name, :backed_projects
 
   def initialize(name)
     @name = name
-    @bracked_projects = []
+    @backed_projects = []
   end
   
   def back_project(project)
-    @bracked_projects << project
+    @backed_projects << project
     project.add_backer(self) unless project.brackers.include?(self)
   end
   
